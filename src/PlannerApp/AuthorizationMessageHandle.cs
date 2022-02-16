dd90af790797bdfda17ad1db62de9001bcc02d6f
ombasamukhwami <mukhwami@gmail.com>
2022-02-16 23:45:34 +0300

    moved AuthorizationMessageHandle to separate file

---
+++ src/PlannerApp/AuthorizationMessageHandle.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using PlannerApp.Shared.Constants;
+using System;
+using System.Net.Http;
+using System.Threading;
+using Blazored.LocalStorage;
+
+namespace PlannerApp
+{
+    public class AuthorizationMessageHandle:DelegatingHandler
+    {
+        private readonly ILocalStorageService _storage;
+
+        public AuthorizationMessageHandle(ILocalStorageService storage)
+        {
+            _storage = storage;
+        }
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+
+            if(await _storage.ContainKeyAsync(PlannerVariables.ACCESS_TOKEN))
+            {
+                var token = await _storage.GetItemAsStringAsync(PlannerVariables.ACCESS_TOKEN);
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(PlannerVariables.BEARER, token);
+            }
+
+            Console.WriteLine("AUTHORIZATIONMESSAGE HANDLER CALLED");
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
--- src/PlannerApp/Program.cs
+++ src/PlannerApp/Program.cs
@@ -5,7 +5,6 @@ using MudBlazor.Services;
 using PlannerApp.Shared.Constants;
 using System;
 using System.Net.Http;
-using System.Threading;
 using Blazored.LocalStorage;
 
 namespace PlannerApp
@@ -32,30 +31,9 @@ namespace PlannerApp
 
 
 
+
+
             await builder.Build().RunAsync();
-        }
-    }
-
-    public class AuthorizationMessageHandle:DelegatingHandler
-    {
-        private readonly ILocalStorageService _storage;
-
-        public AuthorizationMessageHandle(ILocalStorageService storage)
-        {
-            _storage = storage;
-        }
-        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-
-            if(await _storage.ContainKeyAsync(PlannerVariables.ACCESS_TOKEN))
-            {
-                var token = await _storage.GetItemAsStringAsync(PlannerVariables.ACCESS_TOKEN);
-                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(PlannerVariables.BEARER, token);
-            }
-
-            Console.WriteLine("AUTHORIZATIONMESSAGE HANDLER CALLED");
-
-            return await base.SendAsync(request, cancellationToken);
         }
     }
 }
